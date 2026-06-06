71a9099b13d48673327fe87032b42b9c6d003b07
DineshK55 <arasudinesh230@gmail.com>
2026-06-06 14:39:38 +0530

    Production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://bulkmailapp-80cj.onrender.com/api",
 });
 
 export default API;
